ef879f20b3aebd24e272b342891c67d4f16b1f71
Abhishek Mishra <iabm3568@gmail.com>
2026-01-02 20:06:28 +0530

    Refactor Skills component to remove state and input
    Removed state management and input handling from the Skills component.

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,37 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Skills = () => {
-  console.log("React.memo component rendered");
-
-  const [skills, setSkills] = useState([
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React"
-  ]);
-  const [input, setInput] = useState("");
-
-  const addSkill = () => {
-    if (input.length > 5) {
-      setSkills([...skills, input]);
-      setInput("");
-    }
-  };
-
   return (
     <>
       <h2>React.memo</h2>
-
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={addSkill}>Add Skill</button>
-
       <ul>
-        {skills.map((skill, i) => (
-          <li key={i}>{skill}</li>
-        ))}
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
       </ul>
     </>
   );
